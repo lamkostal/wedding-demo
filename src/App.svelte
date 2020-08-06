afddc4fb061f6dfd5c800e9de37b376a12df1f09
lamkostal <lamkostal@gmail.com>
2020-08-06 16:13:21 +0300

    init

---
+++ src/App.svelte
@@ -0,0 +1,126 @@
+<script>
+  import { sources } from "./sources.js";
+  import Card from "./Card.svelte";
+  import { onMount } from "svelte";
+
+  let checked;
+  console.log("checked=" + checked);
+  let entries;
+  let list = [];
+  $: entries = list;
+
+  function pushValues(event) {
+    entries = [...entries, event.detail].filter(function(item, pos, self) {
+      return self.indexOf(item) == pos;
+    });
+  }
+  function removeValues(event) {
+    // const index = entries.indexOf(event.detail);
+    // console.log("now index of is:"+index)
+    // entries.splice(index,1)
+    // console.log("now list is:"+entries)
+    entries = entries.filter(m => m !== event.detail);
+  }
+
+  $: console.log("mylist:" + entries);
+</script>
+
+<style>
+  h1{
+    padding:0 1em;
+  }
+  h2.selected {
+    margin: 0;
+    padding: 20px 5px;
+    position: fixed;
+    top: 0;
+    background: #dadada;
+    font-size: 1.4em;
+  }
+  ul {
+    list-style: none;
+    margin: 100px 0;
+    padding: 0;
+  }
+  li {
+    padding: 10px 10px;
+    border-top: 2px solid #fff;
+    display: flex;
+    align-items: center;
+  }
+  li span {
+    margin: 0 5px;
+  }
+  .aside {
+    scroll-behavior: auto;
+    width: 300px;
+    height: 100vh;
+    position: fixed;
+    background: #dadada;
+    border-right: 1px solid grey;
+    top: 0;
+    left: 0;
+    overflow:auto;
+  }
+.container{
+	margin:0 0 0 300px;
+}
+  .grid-container {
+    padding:20px;
+    
+  }
+  .grid {
+    position: static;
+
+    display: grid;
+    grid-column-gap: 20px;
+    grid-row-gap: 25px;
+
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  }
+  .submit{
+    position: fixed;
+    top:50px;
+    left: 5px;
+    z-index: 1;
+  }
+</style>
+
+<main>
+
+    <div class="container">
+    	<h1>Γιωργος & Φωτεινή</h1>
+	
+	    <div class="grid-container">
+	      <form action="" name="selection" id="selection" method="POST">
+	        <div class="grid">
+	          {#each sources as source, i}
+	            <Card
+	              on:add={pushValues}
+	              on:remove={removeValues}
+	              src={source.src}
+	              name={'photo' + i} />
+	          {/each}
+	        </div>
+	        <input type="submit" value="Submit!" class="submit" />
+	      </form>
+	
+	    </div>
+    </div>
+    <div class="aside">
+      <ul>
+        <h2 class="selected">Selected Photos {entries.length} / 100</h2>
+        {#each entries as item, i}
+          <li>
+            <span>{"#" + (i+1)}</span>
+            <img src={item} alt="img-thumb" width="65" />
+            <span>{' ' + item.substring(13)}</span>
+          </li>
+        {/each}
+      </ul>
+    </div>
+
+  
+
+</main>
+
---
+++ src/Card.svelte
@@ -0,0 +1,86 @@
+<script>
+import {createEventDispatcher} from "svelte";
+const dispatch = new createEventDispatcher();
+
+
+ 
+export let src,name
+let checked = false;
+let checks
+$: checks=!checked
+
+
+function sendMessage(){
+S: console.log("checks from comp:"+" "+ checks)
+
+    if(checks){dispatch('add',this.value)
+    console.log("adds")}
+    else{
+        dispatch('remove',this.value)
+        console.log("removes")
+        }
+
+}
+S: console.log("checked from comp:"+" "+ checked)
+// S: console.log("checks from comp:"+" "+ checks)
+
+
+
+</script>
+
+<div class="card">
+  
+  <input class="checkbox" type="checkbox" name={name} bind:checked={checked} on:input={sendMessage} value="{src}">
+  
+ 
+  <img src="{src}" alt="" class="photo">
+  <span class="file-name">{src.substring(13)}</span>
+    
+</div>
+
+<style>
+.photo{
+    width:100%;
+    padding: 0px;
+    transition:0.2s ease;
+    border-radius: 2px;
+}
+.file-name{
+    position: relative;
+    padding: 5px;
+    
+}
+.checkbox:checked + img{
+    background: green;
+    padding: 5px;
+
+}
+.checkbox{
+    -webkit-appearance: none;
+    border: 1px solid #636363;
+	box-shadow: 0 1px 2px rgba(0,0,0,0.1), inset 0px -15px 10px -12px rgba(0,0,0,0.05);
+	padding: 9px;
+	border-radius: 3px;
+	display: inline-block;
+    position: relative;
+    margin: 0;
+   
+}
+.checkbox:checked {
+    background: green;
+    border: 1px solid #acacac;
+    outline:none;
+
+}
+.checkbox:active, .checkbox:focus{
+     outline:none;
+}
+.checkbox:checked:after {
+	content: '\2714';
+	font-size: 14px;
+	position: absolute;
+	top: 0px;
+	left: 3px;
+	color: #ffffff;
+}
+</style>
